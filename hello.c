43120f3593448da848d92e7c5af320347d889779
Joshua Ferugson <jferguson32@huskers.unl.edu>
2025-08-29 17:42:29 -0500

    added my major too

--- hello.c
+++ hello.c
@@ -9,6 +9,9 @@
     printf("Hello, I'm Case Henke.\n");
     printf("Major is Computer Engineering!\n");
     printf("I do not have a minor\n")
+    printf("Hello, my name is Joshua Ferguson.\n")
+    printf("My major is Electrical Engineering!\n")
+
 
     return 0;
 }
